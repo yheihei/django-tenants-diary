3bbfeef5a1539a9e86ef0cf48053d2bf7720b10b
Yohei Kokubo <yhei.register@gmail.com>
2021-06-02 14:09:34 +0900

    コメント追加 - プレミアムユーザー機能のテストを追加する #16

--- django_tenants_diary/diary/tests.py
+++ django_tenants_diary/diary/tests.py
@@ -8,13 +8,14 @@ class PremiumUserTest(TenantTestCase):
   @classmethod
   def setup_tenant(cls, tenant):
     """
-    テスト用テナントの設定をここで行う
+    テスト用テナントの設定
     """
     tenant.is_premium = True
     return tenant
 
   def setUp(self) -> None:
     super().setUp()
+    # テスト用テナントでアクセスするためのクライアント
     self.c = TenantClient(self.tenant)
   
   def tearDown(self) -> None:
