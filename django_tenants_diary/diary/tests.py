ed9d24495e3e2c527cb9d3432173a2a4a93d1bc8
Yohei Kokubo <yhei.register@gmail.com>
2021-06-02 09:25:11 +0900

    プレミアムユーザー機能のテストを追加する #16

--- django_tenants_diary/diary/tests.py
+++ django_tenants_diary/diary/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django_tenants.test.cases import TenantTestCase
+from django_tenants.test.client import TenantClient
+from django.urls import reverse
+from tenants.models import Tenant
 
-# Create your tests here.
+class PremiumUserTest(TenantTestCase):
+  
+  @classmethod
+  def setup_tenant(cls, tenant):
+    """
+    テスト用テナントの設定をここで行う
+    """
+    tenant.is_premium = True
+    return tenant
+
+  def setUp(self) -> None:
+    super().setUp()
+    self.c = TenantClient(self.tenant)
+  
+  def tearDown(self) -> None:
+    pass
+
+  def test_is_premium(self):
+    '''プレミアムユーザーのテナントのトップページを開いたときプレミアムフラグがついている事'''
+    response = self.c.get(reverse('diary:index'))
+    print(response.status_code)
+    self.assertEqual(True, response.context['tenant'].is_premium)
